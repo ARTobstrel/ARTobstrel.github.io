c206737407889a739baf99415a6ba5ebe7104082
Artem <artemfap@yandex.ru>
2019-10-05 01:04:45 +0300

    add new app
    weather app

---
+++ weather_app/precache-manifest.5eab3118d628fda8faf7e9d56fa162f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ac291421623a8689e46610d000e2be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "280d9229027ffc35c83e",
+    "url": "/static/css/main.68365184.chunk.css"
+  },
+  {
+    "revision": "701804f7ef38e61335dd",
+    "url": "/static/js/2.54b8a50f.chunk.js"
+  },
+  {
+    "revision": "280d9229027ffc35c83e",
+    "url": "/static/js/main.c95760ce.chunk.js"
+  },
+  {
+    "revision": "58f2d07760f13ffe89b9",
+    "url": "/static/js/runtime~main.7885dd74.js"
+  }
+]);
